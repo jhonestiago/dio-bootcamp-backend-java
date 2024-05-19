ff017d4adf66507cc97cb99fa25c034081f01974
jhonestiago <jonestiago.94@gmail.com>
2024-05-19 17:52:36 -0300

    classe Aluno

--- modulos/modulo-3/java-orientacao-objetos/java-getter-setter/src/escola/Aluno.java
+++ modulos/modulo-3/java-orientacao-objetos/java-getter-setter/src/escola/Aluno.java
@@ -2,7 +2,27 @@ package escola;
 
 public class Aluno {
 
-    String nome;
-    int idade;
+    private String nome;
+    private int idade;
+
+    // nome.getter
+    public String getNome() {
+        return nome;
+    }
+
+    // nome.setter
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    // idade.getter
+    public int getIdade() {
+        return idade;
+    }
+
+    // idade.setter
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
 
 }
