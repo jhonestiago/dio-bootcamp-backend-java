39459828ec2cb893ac1558ffea8cb6feda27e216
jhonestiago <jonestiago.94@gmail.com>
2024-05-20 14:06:43 -0300

    classe SistemaCadastro

--- modulos/modulo-3/java-orientacao-objetos/java-construtor-classe/src/cadastro/SistemaCadastro.java
+++ modulos/modulo-3/java-orientacao-objetos/java-construtor-classe/src/cadastro/SistemaCadastro.java
@@ -4,7 +4,7 @@ public class SistemaCadastro {
 
     public static void main(String[] args) {
         
-        Pessoa marcos = new Pessoa();
+        Pessoa marcos = new Pessoa("MARCOS SILVA", "01078900178");
 
         marcos.setEndereco("Rua das Marias");
 
