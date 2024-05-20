264eb2cdee01ab2bcb172a255b0126bbd1bbd6a0
jhonestiago <jonestiago.94@gmail.com>
2024-05-20 12:53:35 -0300

    classe Pessoa

---
+++ modulos/modulo-3/java-orientacao-objetos/java-construtor-classe/src/cadastro/Pessoa.java
@@ -0,0 +1,35 @@
+package cadastro;
+
+public class Pessoa {
+
+    private String nome;
+    private String cpf;
+    private String endereco;
+
+    // nome.getter
+    public String getNome() {
+        return nome;
+    }
+
+    // nome.setter
+    // ?
+
+    // cpf.getter
+    public String getCpf(){
+        return cpf;
+    }
+
+    // cpf.setter
+    // ?
+
+    // endereco.getter
+    public String getEndereco() {
+        return endereco;
+    }
+
+    // endereco.setter
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+}
