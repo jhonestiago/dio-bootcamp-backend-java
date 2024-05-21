3d8e16de9ea630df6e4bf00cd9754dca2d7f2a37
jhonestiago <jonestiago.94@gmail.com>
2024-05-21 16:18:40 -0300

    abstracao

--- modulos/modulo-3/java-orientacao-objetos/java-pilares-poo/src/ServicoMensagemInstantanea.java
+++ modulos/modulo-3/java-orientacao-objetos/java-pilares-poo/src/ServicoMensagemInstantanea.java
@@ -1,21 +1,7 @@
-public class ServicoMensagemInstantanea {
+public abstract class ServicoMensagemInstantanea {
     
-    public void enviarMensagem() {
-        validarConectadoInternet();
-        System.out.println("Enviando mensagem");
-        salvarHistoricoMensagem();
-    }
+    public abstract void enviarMensagem();
 
-	public void receberMensagem() {
-		System.out.println("Recebendo mensagem");
-	}
-
-	private void validarConectadoInternet() {
-		System.out.println("Validando se está conectado a internet");
-	}
-
-	private void salvarHistoricoMensagem() {
-		System.out.println("Salvando o histórico da mensagem");
-    }
+	public abstract void receberMensagem();
 
 }
