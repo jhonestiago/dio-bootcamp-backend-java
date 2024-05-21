cc77177246a0d5d2694c4364379376906d192364
jhonestiago <jonestiago.94@gmail.com>
2024-05-21 16:25:44 -0300

    abstração

--- modulos/modulo-3/java-orientacao-objetos/java-pilares-poo/src/Telegram.java
+++ modulos/modulo-3/java-orientacao-objetos/java-pilares-poo/src/Telegram.java
@@ -1,1 +1,14 @@
-public class Telegram extends ServicoMensagemInstantanea {}
+public class Telegram extends ServicoMensagemInstantanea {
+
+    @Override
+	public void enviarMensagem() {
+	    System.out.println("Enviando mensagem pelo Telegram Messenger");
+	}
+
+    @Override
+	public void receberMensagem() {
+	    System.out.println("Recebendo mensagem pelo Telegram Messenger");
+	}
+
+
+}
