c5435f578f34705f384f35140ac1d6fe7f288a32
jhonestiago <jonestiago.94@gmail.com>
2024-05-21 12:01:54 -0300

    ComputadorPedrinho

---
+++ modulos/modulo-3/java-orientacao-objetos/java-pilares-poo/src/ComputadorPedrinho.java
@@ -0,0 +1,16 @@
+public class ComputadorPedrinho {
+
+    public static void main(String[] args) {
+
+        //abrindo MSN Messenger
+
+        MSNMessenger msn = new MSNMessenger();
+		
+		msn.validarConectadoInternet();
+		msn.enviarMensagem();
+		msn.salvarHistoricoMensagem();
+		
+		msn.receberMensagem();
+	}
+
+}
