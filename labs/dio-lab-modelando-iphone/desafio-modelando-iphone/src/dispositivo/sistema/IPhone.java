4b6bdb81a97ffdd643ade9a05ccb293ef1ebf03c
jhonestiago <jonestiago.94@gmail.com>
2024-05-22 01:13:10 -0300

    classe IPhone

--- labs/dio-lab-modelando-iphone/desafio-modelando-iphone/src/dispositivo/sistema/IPhone.java
+++ labs/dio-lab-modelando-iphone/desafio-modelando-iphone/src/dispositivo/sistema/IPhone.java
@@ -2,42 +2,91 @@ package dispositivo.sistema;
 
 import dispositivo.aplicativos.ReprodutorMusical;
 import dispositivo.aplicativos.AparelhoTelefonico;
+import dispositivo.aplicativos.Navegador;
 
-public class IPhone implements ReprodutorMusical, AparelhoTelefonico {
+public class IPhone implements ReprodutorMusical, AparelhoTelefonico, Navegador {
+
+    private String estadoDispositivo = "Ligado";
+    private int volume = 0;
     
+    
+    // Metodos IPhone
+    public void ligarDispositivo() {
+        if (this.estadoDispositivo == "Desligado") {
+            this.estadoDispositivo = "Ligado";
+            System.out.println("Ligando iPhone... Aguarde.");
+        }
+    }
+
+    public void desligarDispositivo() {
+        if (this.estadoDispositivo == "Ligado") {
+            this.estadoDispositivo = "Desligado";
+            System.out.println("Desligando iPhone...");
+        }
+    }
+
+    public void aumentarVolume() {
+        if (volume < 25) {
+            volume++;
+        }
+        System.out.println("Volume aumentado para: " + volume);
+    }
+
+    public void diminuirVolume() {
+        if (volume > 0) {
+            volume--;
+        }
+        System.out.println("Volume diminuído para: " + volume);
+    }
+    
+    // Médodos Aparelho Telefonico
     @Override
     public void ligar(String numero) {
-        System.out.println("Chamando " + numero);
+        System.out.println("Efetuando chamada para: " + numero);
     }
    
     @Override
     public void atenderChamada() {
-        System.out.println("Atendendo Chamada");
+        System.out.println("Chamada recebida. Atendedendo...");
     }
     
     @Override
     public void encerrarChamada() {
-        System.out.println("Chamada encerrada");
+        System.out.println("Chamada encerrada com sucesso.");
     } 
     
     @Override
     public void iniciarCorreioVoz() {
-        System.out.println("Chamando Correio de Voz");
+        System.out.println("Redirecionando para Correio de Voz...");
     } 
 
+    // Métodos Navegador
+    public void exibirPagina(String url) {
+        System.out.println("Abrindo página: " + url);
+    }
+
+    public void adicionarNovaAba() {
+        System.out.println("Nova aba criada com sucesso.");
+    }
+
+    public void atualizarPagina() {
+        System.out.println("Recarregando página atual...");
+    }
+
+    // Métodos Reprodutor Musical
     @Override
     public void tocarMusica() {
-        System.out.println("Reproduzindo");
+        System.out.println("Reproduzindo música...");
     }
 
     @Override
     public void pausarMusica() {
-        System.out.println("Em pausa");
+        System.out.println("Música pausada.");
     }
 
     @Override
     public void selecionarMusica(String musica) {
-        System.out.println("Reproduzindo " + musica);
+        System.out.println("Selecionando música: " + musica + ". Começando a reprodução...");
     }
 
 }
