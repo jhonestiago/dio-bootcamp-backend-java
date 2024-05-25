513b8b40c20fbb70f8e2b9eb9cac0d18a72b6278
jhonestiago <jonestiago.94@gmail.com>
2024-05-24 22:30:28 -0300

    Pesquisa

---
+++ trilha-java-basico/modulo-3/java-collections/java-collections-api/src/list/Pesquisa/CatalogoLivros/Biblioteca/CatalogoLivros.java
@@ -0,0 +1,52 @@
+package list.Pesquisa.CatalogoLivros.Biblioteca;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class CatalogoLivros {
+
+    private List<Livro> livros;
+
+    public CatalogoLivros() {
+        this.livros = new ArrayList<>();
+    }
+    
+    public void adicionarLivro(String titulo, String autor, int anoPublicacao) {
+        this.livros.add(new Livro(titulo, autor, anoPublicacao));
+    }
+
+    public List<Livro> pesquisarPorAutor(String autor) {
+        List<Livro> livrosPorAutor = new ArrayList<>();
+        if (!this.livros.isEmpty()) {
+            for (Livro livro : this.livros) {
+                if (livro.getAutor().equalsIgnoreCase(autor)) {
+                    livrosPorAutor.add(livro);
+                }
+            }
+        }
+        return livrosPorAutor;
+    }
+
+    public List<Livro> pesquisaPorIntervaloAnos(int anoInicial, int anoFinal) {
+        List<Livro> livrosPorIntervaloAnos = new ArrayList<>();
+        for (Livro livro : this.livros) {
+            int anoPublicacao = livro.getAnoPublicacao();
+            if (anoPublicacao >= anoInicial && anoPublicacao <= anoFinal) {
+                livrosPorIntervaloAnos.add(livro);
+            }
+        }
+        return livrosPorIntervaloAnos;
+    }
+
+    public Livro pesquisarPorTítulo(String titulo) {
+        Livro livroPorTitulo = null;
+        for (Livro livro : this.livros) {
+            if (livro.getTitulo().equalsIgnoreCase(titulo)) {
+                livroPorTitulo = livro;
+                break;
+            }
+        }
+        return livroPorTitulo;
+    }
+
+}
